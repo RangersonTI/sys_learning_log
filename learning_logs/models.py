85e77c49252b2addcb4e44fceda25d562b1759ce
Rangerson Clemente <rangerson14@outlook.com>
2024-03-30 18:11:12 -0300

    update

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Topic(models.Model):
+    # Um novo assunto a aprender
+    text = models.CharField(max_length = 100)
+    date_created = models.DateTimeField(auto_now_add = True)
+    
+    def __str__(self):
+        # Devolve a representação e String do modelo
+        return self.text
+    
+class Entry(models.Model):
+    # Algo especifico aprendido sobre um assunto
+    topic = models.ForeignKey(Topic, on_delete=  models.CASCADE)
+    text = models.CharField(max_length = 500)
+    date_created = models.DateTimeField(auto_now_add = True)
+    
+    def __str__(self):
+        return self.text[:50]+"..."
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+def index(request):
+    """Pagina principal da Aplicação"""
+    return render(request, 'learning_logs/index.html')
 # Create your views here.
