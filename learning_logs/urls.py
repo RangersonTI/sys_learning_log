b640bb535e618c4e3e921d5852c85966cb5cda92
Rangerson Clemente <rangerson14@outlook.com>
2024-03-30 21:53:31 -0300

    update

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('check_topics/', views.topics, name="check_topics"),
+    path('topic/<topic_id>/', views.topic, name='topic'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from .models import Topic
 
 def index(request):
     """Pagina principal da Aplicação"""
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    topic = Topic.objects.order_by('date_created')
+    context = {'topics':topic}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id = topic_id)
+    entries = topic.entry_set.order_by('-date_created')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
+
+
 # Create your views here.
